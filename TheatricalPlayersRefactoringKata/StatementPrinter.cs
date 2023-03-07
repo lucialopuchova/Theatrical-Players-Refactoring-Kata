e2d11e8af36f42c1cc991075888a38449436f337
jakubvanko <kubko.vanko@gmail.com>
2023-03-07 19:38:34 +0100

    Renamed variables

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -18,23 +18,21 @@ namespace TheatricalPlayersRefactoringKata
             foreach (var perf in invoice.Performances)
             {
                 var play = plays[perf.PlayID];
-                var thisAmount = CalculatePerformanceRevenue(play.Type, perf.Audience);
+                var revenue = CalculatePerformanceRevenue(play.Type, perf.Audience);
 
-                // add volume credits
                 volumeCredits += CalculateVolumeCredits(play.Type, perf.Audience);
 
-                // print line for this order
-                resultStringBuilder.Append(OutputOrderFormat(cultureInfo, play, thisAmount, perf));
-                totalAmount += thisAmount;
+                resultStringBuilder.Append(OutputOrderFormat(cultureInfo, play, revenue, perf));
+                totalAmount += revenue;
             }
             resultStringBuilder.Append(string.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100)));
             resultStringBuilder.Append(string.Format("You earned {0} credits\n", volumeCredits));
             return resultStringBuilder.ToString();
         }
 
-        private static string OutputOrderFormat(CultureInfo cultureInfo, Play play, int thisAmount, Performance perf)
+        private static string OutputOrderFormat(CultureInfo cultureInfo, Play play, int revenue, Performance perf)
         {
-            return string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
+            return string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(revenue / 100), perf.Audience);
         }
 
         private int CalculateVolumeCredits(string playType, int audience)
@@ -48,10 +46,10 @@ namespace TheatricalPlayersRefactoringKata
             return output;
         }
 
-        private int CalculatePerformanceRevenue(string type, int audience)
+        private int CalculatePerformanceRevenue(string playType, int audience)
         {
             int revenue = 0;
-            switch (type)
+            switch (playType)
             {
                 case "tragedy":
                     revenue = 40000;
@@ -69,7 +67,7 @@ namespace TheatricalPlayersRefactoringKata
                     revenue += 300 * audience;
                     break;
                 default:
-                    throw new Exception("unknown type: " + type);
+                    throw new Exception("unknown type: " + playType);
             }
 
             return revenue;
