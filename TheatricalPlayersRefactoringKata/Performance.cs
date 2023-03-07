d1c5f4ca0494f52ac5a2cb181336c125aca306e1
FilipHujer <filip.hujer1@gmail.com>
2023-03-07 19:31:25 +0100

    Rollback Performance.cs

--- TheatricalPlayersRefactoringKata/Performance.cs
+++ TheatricalPlayersRefactoringKata/Performance.cs
@@ -5,13 +5,13 @@ namespace TheatricalPlayersRefactoringKata
         private string _playID;
         private int _audience;
 
-        public string PlayID => _playID;
-        public int Audience => _audience;
+        public string PlayID { get => _playID; set => _playID = value; }
+        public int Audience { get => _audience; set => _audience = value; }
 
         public Performance(string playID, int audience)
         {
-            _playID = playID;
-            _audience = audience;
+            this._playID = playID;
+            this._audience = audience;
         }
 
     }
